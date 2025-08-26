377b62ebe830af74402047522d99b95ad92744f6
BonannoPablo <bonannopabloariel@gmail.com>
2025-08-26 15:19:53 -0300

    Added state transition table and completed Token class and interface

--- src/compiler/IToken.java
+++ src/compiler/IToken.java
@@ -1,5 +1,10 @@
 package compiler;
 
 public interface IToken {
+    public static enum TokenType {
+        METVARID, CLASSID, INTLITERAL, CHARLITERAL, STRINGLITERAL, EOF
+    }
     public String getTokenValue();
+    public TokenType getTokenType();
+    public int getLineNumber();
 }
--- src/compiler/Token.java
+++ src/compiler/Token.java
@@ -1,14 +1,29 @@
 package compiler;
 
-public class Token implements IToken{
-    String tokenValue;
+public class Token implements IToken {
 
-    public Token(String tokenValue){
+    private String tokenValue;
+    private TokenType tokenType;
+    private int lineNumber;
+
+    public Token(String tokenValue, TokenType tokenType, int lineNumber){
         this.tokenValue = tokenValue;
+        this.tokenType = tokenType;
+        this.lineNumber = lineNumber;
     }
 
     @Override
     public String getTokenValue() {
         return tokenValue;
+    }
+
+    @Override
+    public TokenType getTokenType() {
+        return tokenType;
+    }
+
+    @Override
+    public int getLineNumber() {
+        return lineNumber;
     }
 }
